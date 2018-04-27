dbd8d54339be99b0e63cdc55f22e8f172af35a36
Scott Rudiger <scottrudiger@gmail.com>
2018-04-26 23:44:04 -0700

    feat(title): allow setting mainpagetitle in jsdoc.conf.*.opts so it shows up instead of Home (#12)
    * "feat(title): allow setting mainpagetitle in jsdoc.conf.*.opts so it shows up instead of Home"
    
    * empty commit
    
    * "fix: remove redundant page-title from index.html"
    
    * empty commit

--- publish.js
+++ publish.js
@@ -207,13 +207,14 @@ function getPathFromDoclet(doclet) {
         doclet.meta.filename;
 }
 
-function generate(type, title, docs, filename, resolveLinks) {
+function generate(type, title, docs, filename, resolveLinks, isIndex) {
     resolveLinks = resolveLinks === false ? false : true;
 
     var docData = {
         type: type,
         title: title,
-        docs: docs
+        docs: docs,
+        isIndex: isIndex
     };
 
     var outpath = path.join(outdir, filename),
@@ -609,11 +610,11 @@ exports.publish = function(taffyData, opts, tutorials) {
     var files = find({kind: 'file'});
     var packages = find({kind: 'package'});
 
-    generate('', 'Home',
+    generate('', opts.mainpagetitle || 'Home',
         packages.concat(
             [{kind: 'mainpage', readme: opts.readme, longname: (opts.mainpagetitle) ? opts.mainpagetitle : 'Main Page'}]
         ).concat(files),
-    indexUrl);
+    indexUrl, undefined, true);
 
     // set up the lists that we'll use to generate pages
     var classes = taffy(members.classes);
